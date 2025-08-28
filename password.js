27a3216b0b57acf125d0cd524383271c98c10220
Navya P <navyap304@gmail.com>
2025-08-29 00:30:29 +0530

    Add files via upload

---
+++ password.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const passwordSchema = new mongoose.Schema({
+  website: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+}, { timestamps: true });
+
+const Password = mongoose.model("Password", passwordSchema);
+
+export default Password;
